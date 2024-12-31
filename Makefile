9c5c59d07acac72b3b3c9a496d59696f4a51996a
Ulises Jimenez <ulisesjimenez@google.com>
2024-12-31 13:13:54 -0600

    initial template commit

---
+++ Makefile
@@ -0,0 +1,15 @@
+# Execute the "targets" in this file with `make <target>` e.g., `make test`.
+#
+# You can also run multiple in sequence, e.g. `make clean lint test serve-coverage-report`
+
+clean:
+	bash run.sh clean
+
+help:
+	bash run.sh help
+
+install:
+	bash run.sh install
+
+generate-project:
+	bash run.sh generate-project	
---
+++ tests/test__generate_projecty.py
@@ -0,0 +1,32 @@
+import json
+import subprocess
+from pathlib import Path
+
+THIS_DIR = Path(__file__).parent
+PROJECT_DIR = (THIS_DIR / "../").resolve()
+
+def test__can_generate_project():
+    """
+
+    execute: `cookiecutter <template directory> ...`
+    """
+    cookiecutter_config = {
+        "default_context" : {"repo_name": "test-repo"} 
+    }
+    cookiecutter_config_fpath = PROJECT_DIR / "cookiecutter-test-config.json"
+    cookiecutter_config_fpath.write_text(json.dumps(cookiecutter_config))
+
+    cmd = [
+        "cookiecutter",
+        str(PROJECT_DIR),
+        "--output-dir",
+        str( PROJECT_DIR /"sample"),
+        "--no-input",
+        "--config-file",
+        str(cookiecutter_config_fpath)
+    ]
+    print("COMMAND: ", " ".join(cmd))
+    subprocess.run(cmd, check=True )
+
+    generated_project_dir = PROJECT_DIR / "sample" / cookiecutter_config["default_context"]["repo_name"]
+    assert generated_project_dir.exists()
